43f012f574a1323b07e6512a368fc6d3b1e707eb
edcalson <edacir@gmail.com>
2024-09-29 23:46:22 -0300

    Update ContaCorrente.java

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -1,12 +1,21 @@
 public class ContaCorrente {
     private double saldo;
 
+    public double getSaldo(){
+        return this.saldo;
+    }
+
     public ContaCorrente(double saldoInicial) {
         this.saldo = saldoInicial;
     }
 
-    public void sacar(double valor) {
+    public void sacar(double valor) throws SaldoInsuficienteException {
+        if (this.saldo - valor < 0){
+            throw new SaldoInsuficienteException(String.format(
+                    "Você está tentando sacar R$ %.2f, mas você tem R$ %.2f na conta",
+                    valor, this.saldo));
+        }
+
         this.saldo -= valor;
-        
     }
 }
